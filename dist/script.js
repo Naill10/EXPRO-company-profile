3bc2c56bb30e494877c60583c94ab025f0b26b60
Naill10 <abbadnailun28ganz@gmail.com>
2025-11-06 15:42:25 +0700

    Perfection

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,20 @@
-let btn = document.getElementById('btn')
-let project = document.getElementById('project')
+// let btn = document.getElementById('btn')
+// let project = document.getElementById('project')
+let service = document.getElementById('service')
 
-window.addEventListener('scroll', function(){
-    let value = this.window.scrollY;
-})
+// window.addEventListener('scroll', function(){
+//     let value = this.window.scrollY;
+// })
+
+
+window.addEventListener('scroll',function(){
+const navbar = document.getElementById('navbar');
+
+if (window.scrollY > 0){
+    navbar.classList.add("scrolled")
+
+}
+else{
+    (navbar.classList.remove("scrolled"))
+}
+});
